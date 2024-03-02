926761661fad9976851565cb5615ee216cd65d36
Jose Conchello <jose.conchello@gmail.com>
2024-03-02 14:35:28 -0800

    handle going through door with object

--- week6-zelda/assignment5/src/states/entity/player/PlayerCarryItemWalkState.lua
+++ week6-zelda/assignment5/src/states/entity/player/PlayerCarryItemWalkState.lua
@@ -28,6 +28,80 @@ function PlayerCarryItemWalkState:update(dt)
 
   -- EntityWalkState performs base collision detection against walls
   EntityWalkState.update(self, dt)
+
+
+  -- handle entering another room, drop the item and exit into walk state
+  if self.bumped then
+    if self.entity.direction == 'left' then
+      -- temporarily adjust position into the wall, since bumping pushes outward
+      self.entity.x = self.entity.x - PLAYER_WALK_SPEED * dt
+
+      -- check for colliding into doorway to transition
+      for k, doorway in pairs(self.dungeon.currentRoom.doorways) do
+        if self.entity:collides(doorway) and doorway.open then
+          -- shift entity to center of door to avoid phasing through wall
+          self.entity.y = doorway.y + 4
+          self.entity:dropItem()
+          self.entity:changeState('walk')
+          Event.dispatch('shift-left')
+        end
+      end
+
+      -- readjust
+      self.entity.x = self.entity.x + PLAYER_WALK_SPEED * dt
+    elseif self.entity.direction == 'right' then
+      -- temporarily adjust position
+      self.entity.x = self.entity.x + PLAYER_WALK_SPEED * dt
+
+      -- check for colliding into doorway to transition
+      for k, doorway in pairs(self.dungeon.currentRoom.doorways) do
+        if self.entity:collides(doorway) and doorway.open then
+          -- shift entity to center of door to avoid phasing through wall
+          self.entity.y = doorway.y + 4
+          self.entity:dropItem()
+          self.entity:changeState('walk')
+          Event.dispatch('shift-right')
+        end
+      end
+
+      -- readjust
+      self.entity.x = self.entity.x - PLAYER_WALK_SPEED * dt
+    elseif self.entity.direction == 'up' then
+      -- temporarily adjust position
+      self.entity.y = self.entity.y - PLAYER_WALK_SPEED * dt
+
+      -- check for colliding into doorway to transition
+      for k, doorway in pairs(self.dungeon.currentRoom.doorways) do
+        if self.entity:collides(doorway) and doorway.open then
+          -- shift entity to center of door to avoid phasing through wall
+          self.entity.x = doorway.x + 8
+          self.entity:dropItem()
+          self.entity:changeState('walk')
+          Event.dispatch('shift-up')
+        end
+      end
+
+      -- readjust
+      self.entity.y = self.entity.y + PLAYER_WALK_SPEED * dt
+    else
+      -- temporarily adjust position
+      self.entity.y = self.entity.y + PLAYER_WALK_SPEED * dt
+
+      -- check for colliding into doorway to transition
+      for k, doorway in pairs(self.dungeon.currentRoom.doorways) do
+        if self.entity:collides(doorway) and doorway.open then
+          -- shift entity to center of door to avoid phasing through wall
+          self.entity.x = doorway.x + 8
+          self.entity:dropItem()
+          self.entity:changeState('walk')
+          Event.dispatch('shift-down')
+        end
+      end
+
+      -- readjust
+      self.entity.y = self.entity.y - PLAYER_WALK_SPEED * dt
+    end
+  end
 end
 
 function PlayerCarryItemWalkState:render()
